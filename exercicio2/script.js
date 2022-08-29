dd3bd86a598c927a815e4d1ac2fe47570e26f1b9
marllonrp <marllonrp@yahoo.com.br>
2022-08-29 14:56:26 -0300

    Exercícios diários.

---
+++ exercicio2/script.js
@@ -0,0 +1,17 @@
+const pergunta1 = "Você está vivo?"
+const pergunta2 = "Você respira?"
+const pergunta3 = "Você é indiferente?"
+
+const resposta1 = "Sim"
+const resposta2 = "Sim"
+const resposta3 = "Não"
+
+console.log (resposta1, resposta2, resposta3)
+
+const resposta1a = prompt(pergunta1)
+const resposta2a = prompt(pergunta2)
+const resposta3a = prompt(pergunta3)
+
+console.log( pergunta1, "-", resposta1)
+console.log( pergunta2, "-", resposta2)
+console.log( pergunta3, "-", resposta3)
---
+++ exercicio3/script.js
@@ -0,0 +1,10 @@
+let a = 10 
+let b = 25
+
+let c = a
+let d = b
+
+a = d
+b = c
+
+console.log (a,b)
